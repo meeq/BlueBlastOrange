bc6e08cb2ec706668cbac59dff1a1c4551bb61c2
Christopher Bonhage <me@christopherbonhage.com>
2017-06-22 22:33:12 -0400

    Resolve compilation errors
    Run-time errors remain

--- Engine_Random.cpp
+++ Engine_Random.cpp
@@ -6,7 +6,12 @@ T rand_range(T a, T b)
 	return a + (T) (((double)(b-a+1))*rand()/(RAND_MAX+1.0));
 }
 
+int Random::Range( int a, int b)
+{
+    return rand_range<int>(a, b);
+}
+
 float Random::Range( double a, double b)
 {
-	return a + (float) ( ((double)(b-a+1))*rand()/(RAND_MAX+1.0));
+    return rand_range<double>(a, b);
 }
